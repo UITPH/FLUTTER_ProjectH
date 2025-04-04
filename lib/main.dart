8b79c0ed24f9ab3844fcc723d7103bb3f53b5e4c
hieutran890j2 <anhkhoa200511@gmail.com>
2025-04-04 16:43:03 +0700

    reinitialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_projecth/screens/home_screen.dart';
-import 'package:flutter_projecth/providers/message_provider.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => MessageProvider()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
+//àkạds;fkjadsf;
